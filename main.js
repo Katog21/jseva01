f5c397b90d9f6d4c7e3e33bf4632b6d55c5dfb55
Katog21 <gerato217@gmail.com>
2023-12-13 20:47:52 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// creamos la funcion para calcular el precio de cantidad de entradas
+function buyTicket () {
+    
+    //creamos la constante para calcular el total a pagar
+    const priceTicket = 9.99;
+ 
+    //le pedimos cuantos tickets quiere comprar
+    let numbTicket = Number(prompt("How many Tickets :) ?"));
+    let nameTicket = prompt("do you want rename your tickets? Yes or No").toLowerCase();
+    
+    //condicional para decidir si renonmbra el ticket
+
+    if (nameTicket === "yes"){
+    //bucle por cantidad de ticket ingresados se solicita el nombre
+    for ( let name = 1; name <= numbTicket; name++) {
+        let nombre = prompt("enter your Name");
+
+        alert (`your 1st ticket is assigned to ${nombre}`) 
+    }
+ }
+    //calculamos el costo
+    let totalCost = numbTicket * priceTicket;    
+     // crear un condicional para limitar el costo y la entrada
+     if (numbTicket && priceTicket > 0 ) {
+        alert (`Your cost for ${numbTicket} tickets : $ ${totalCost}`)
+     } 
+     else{
+        alert ("please enter valid keys");
+     }
+
+}
+  buyTicket(); 
+  
+
